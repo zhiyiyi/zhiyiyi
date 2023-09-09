d264a1f6de3d02c98a608e1fec5b5fca6f99e274
itsoknottobeok <36960178+itsoknottobeok@users.noreply.github.com>
2023-09-08 21:59:14 -0700

    Organize .css files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from "react";
 import Preloader from "./Components/Preload";
+import Particle from "./Components/Particle";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import About from "./Components/About";
+import Experience from "./Components/Experience";
 import {
   BrowserRouter as Router,
 } from "react-router-dom";
-import "./style.css";
-import "./App.css";
+import "./Styles/About.css"
+import "./Styles/App.css";
+import "./Styles/Home.css";
+import "./Styles/Navbar.css";
+import "./Styles/Preload.css";
+import "./Styles/Style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Particle from "./Components/Particle";
-import Experience from "./Components/Experience";
+
 
 function App() {
   const [load, upadateLoad] = useState(true);
